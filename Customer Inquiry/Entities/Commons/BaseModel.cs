d38e922ad3ae67aae15750ba1cce9ad8e7c3e2a4
Muiz Junaid Khan <mzkhan25@hotmail.com>
2019-01-23 19:48:05 +0500

    Basic Folder Structuring and Base Model

---
+++ Customer Inquiry/Entities/Commons/BaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Entities.Commons
+{
+    public class BaseModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateModified { get; set; }
+        public string AddedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
