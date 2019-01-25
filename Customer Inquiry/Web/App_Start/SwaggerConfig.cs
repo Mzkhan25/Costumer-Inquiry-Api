c38c5b4c507c4672cf33e5d3a9d1f5c72c26f772
Muiz Junaid Khan <mzkhan25@hotmail.com>
2019-01-25 13:17:43 +0500

    Inquiry Page removed

--- Customer Inquiry/Web/App_Start/SwaggerConfig.cs
+++ Customer Inquiry/Web/App_Start/SwaggerConfig.cs
@@ -32,7 +32,7 @@ namespace Web
                         // hold additional metadata for an API. Version and title are required but you can also provide
                         // additional fields by chaining methods off SingleApiVersion.
                         //
-//                        c.SingleApiVersion("v1", "Web");
+                        c.SingleApiVersion("v1", "Swagger Documentation for Customer Inquiry");
 
                         // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                         //
